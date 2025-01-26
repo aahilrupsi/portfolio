20beded7fb19198b729cac974698719cfe56f730
aahilrupsi <75289210+aahilrupsi@users.noreply.github.com>
2025-01-25 23:29:33 -0800

    lab 3 changes all at once whooo

---
+++ global.js
@@ -0,0 +1,73 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+
+
+
+let pages = [
+  { url: 'https://aahilrupsi.github.io/portfolio/', title: 'About' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'contact/resumepage.html', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url; 
+  }
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  a.toggleAttribute('target', a.host !== location.host);
+
+
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="color-scheme-selector">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+const select = document.querySelector("#color-scheme-selector");
+
+if ("colorScheme" in localStorage) {
+  const savedScheme = localStorage.colorScheme;
+  document.documentElement.style.setProperty("color-scheme", savedScheme);
+  select.value = savedScheme;
+}
+
+select.addEventListener("input", function (event) {
+  const newScheme = event.target.value;
+  document.documentElement.style.setProperty("color-scheme", newScheme);
+  localStorage.colorScheme = newScheme; 
+});
+
+
+
+
+
